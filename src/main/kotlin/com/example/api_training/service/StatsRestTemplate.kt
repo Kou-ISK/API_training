53d9557a44233890fc47a758115afb96d6cd287c
k-isaka <isk.rugby@gmail.com>
2023-04-02 14:20:29 +0900

    feat: restTemplate使用のチュートリアル4

--- src/main/kotlin/com/example/api_training/service/StatsRestTemplate.kt
+++ src/main/kotlin/com/example/api_training/service/StatsRestTemplate.kt
@@ -1,26 +1,39 @@
 package com.example.api_training.service
 
-import org.springframework.http.HttpMethod
-import org.springframework.http.ResponseEntity
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.util.UriUtils
 
 // https://dashboard.e-stat.go.jp/static/api
+// https://www.e-stat.go.jp/api/api-info/e-stat-manual
 @Service
 class StatsRestTemplate(
     val restTemplate: RestTemplate,
 ) {
-    fun request(): ResponseEntity<String> {
-        //リクエストの送信
-        val response =
-            restTemplate.exchange(
-                "/getData/?Lang=JP&IndicatorCode=0201010000000010000",
-                HttpMethod.GET,
-                null,
+    @Value("\${e_stat.api.json.APPLICATION_ID}")
+    var appId = ""
+
+    // TODO statsListから任意のstatsDataIdを取得する
+    var statsDataId = "0000081942"
+
+    // TODO レスポンス用のモデルを作成
+    // 統計データ取得
+    fun getStatsData() {
+        val statsResponse =
+            restTemplate.getForObject(
+                "/getStatsData/?appId=${appId}&statsDataId=${statsDataId}", String::class.java
+            )
+        println(statsResponse)
+    }
+
+    // 統計表情報取得
+    fun getStatsList() {
+        val statsList =
+            restTemplate.getForObject(
+                "/getStatsList/?appId=${appId}&surveyYears=1998&statsCode=00200522",
                 String::class.java
             )
-        println(response)
-        println("===================================${response}=================")
-        return response
+        println(statsList)
     }
 }
