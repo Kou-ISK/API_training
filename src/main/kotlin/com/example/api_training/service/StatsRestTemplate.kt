d947ffc60f4cdffe56a69eaaa801e3b0b737f056
k-isaka <isk.rugby@gmail.com>
2023-04-03 22:29:57 +0900

    feat: restTemplate使用のチュートリアル5

--- src/main/kotlin/com/example/api_training/service/StatsRestTemplate.kt
+++ src/main/kotlin/com/example/api_training/service/StatsRestTemplate.kt
@@ -1,9 +1,9 @@
 package com.example.api_training.service
 
+import net.minidev.json.JSONObject
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
-import org.springframework.web.util.UriUtils
 
 // https://dashboard.e-stat.go.jp/static/api
 // https://www.e-stat.go.jp/api/api-info/e-stat-manual
@@ -22,7 +22,7 @@ class StatsRestTemplate(
     fun getStatsData() {
         val statsResponse =
             restTemplate.getForObject(
-                "/getStatsData/?appId=${appId}&statsDataId=${statsDataId}", String::class.java
+                "/getStatsData/?appId=${appId}&statsDataId=${statsDataId}", JSONObject::class.java
             )
         println(statsResponse)
     }
@@ -32,7 +32,7 @@ class StatsRestTemplate(
         val statsList =
             restTemplate.getForObject(
                 "/getStatsList/?appId=${appId}&surveyYears=1998&statsCode=00200522",
-                String::class.java
+                JSONObject::class.java
             )
         println(statsList)
     }
